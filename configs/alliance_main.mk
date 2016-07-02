f9d6d5280ba1a965787c9b257a86f969b0fe23f8
Morningstar <admirethedistance@gmail.com>
2016-07-02 17:40:03 -0400

    Edit rule for using prebuilt apk
    Still not sure this is working quite right

--- configs/alliance_main.mk
+++ configs/alliance_main.mk
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export AC_SRC := false
 
 # Include overlays
 PRODUCT_PACKAGE_OVERLAYS += \
